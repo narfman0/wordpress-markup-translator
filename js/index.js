10265f02ca03e7b8e785713f5b42b5dcd69a725b
Jon Robison <jon.robison@coxinc.com>
2016-03-02 12:13:38 -0500

    -Update and fix some stuff. Support twitter, youtube, soundcloud.

--- js/index.js
+++ js/index.js
@@ -1,19 +1,51 @@
 function WordpressShortcodeTranslator() {
     var methods = {};
-    methods.translate = function(text) {
-        var regex = /\[(soundcloud|youtube|twitter) (.*)\]/g;
-        var matches;
-        while ((matches = regex.exec(text)) !== null) {
-            var msg = 'Found ' + matches[0] + '. ';
-            msg += 'Next match starts at ' + regex.lastIndex;
-            msg += ' matches[1]=' + matches[1] + ' matches[2]=' + matches[2];
-            console.log(msg);
+    methods.loadTwitter = function() {
+        window.twttr = (function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0],
+            t = window.twttr || {};
+          if (d.getElementById(id)) return t;
+          js = d.createElement(s);
+          js.id = id;
+          js.src = "https://platform.twitter.com/widgets.js";
+          fjs.parentNode.insertBefore(js, fjs);
+          t._e = [];
+          t.ready = function(f) {
+            t._e.push(f);
+          };
+          return t;
+        }(document, "script", "twitter-wjs"));
+    };
+    methods.translate = function(html) {
+        function handler(match, type, attrs) {
+            switch(type) {
+                case 'soundcloud':
+                    var target = /url="(\S+)"/.exec(attrs);
+                    var src = "https://w.soundcloud.com/player/?url=" + target[1];
+                    return '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="' + src + '"></iframe>';
+                case 'youtube':
+                    var idRegex = /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/ ]{11})/i;
+                    var youtubeId = idRegex.exec(attrs)[1];
+                    return '<iframe src="http://www.youtube.com/embed/' + youtubeId +
+                        '" type="text/html" width="640" height="390" frameborder="0"></iframe>';
+                case 'twitter-timeline':
+                    var id = /id=(\S+)/.exec(attrs)[1];
+                    var username = /username=(\S+)/.exec(attrs)[1];
+                    return '<a class="twitter-timeline" data-widget-id="' + id +
+                        '" href="https://twitter.com/' + username +
+                        '" data-aria-polite="assertive">Tweets</a>';
+                default:
+                    // if we dont know what it is, kill it
+                    return '';
+            }
         }
+        var regex = /\[(\S+)(.*?)\]/g;
+        var result = html.replace(regex, handler);
+        if(typeof window !== 'undefined' && !window.twttr){
+            methods.loadTwitter();
+        }
+        return result;
     };
-    methods.translateSoundcloud = function(data) {
-        return '<iframe allowtransparency="true" scrolling="no" frameborder="no" src="https://w.soundcloud.com/icon/?url=http%3A%2F%2Fsoundcloud.com%2Fundefined&color=orange_white&size=32" style="width: 32px; height: 32px;"></iframe>';
-    };
-
     if(typeof window !== 'undefined'){
         window.wordpressShortcodeTranslator = methods;
     }
--- test/index.js
+++ test/index.js
@@ -1,12 +1,13 @@
+var assert = require('assert');
 var should = require('chai').should(),
     module = require('../dist/index');
 
 describe('#translate-single', function() {
-    it('Translate single', function() {
+    it('Translate single soundcloud', function() {
         var url = "https://api.soundcloud.com/tracks/245445697/";
-        var html = 'text1 [soundcloud url="' + url + '"] text2';
+        var html = '[soundcloud url="' + url + '"]';
         var newHtml = module.translate(html);
-        var expectedHtml = 'text1 <iframe src="' + url + '"></iframe> text2';
-        newHtml.should.equal(expectedHtml);
+        assert(newHtml.indexOf('<iframe') != -1);
+        assert(newHtml.match(/src=\S+url=\S+api.soundcloud.com.tracks.[0-9]+/));
     });
 });
